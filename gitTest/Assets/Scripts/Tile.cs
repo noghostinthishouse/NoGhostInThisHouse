ab4d95e6998b5e204e15fc244b6dc21439c7a16c
wapee <wapee_o@sci.tu.ac.th>
2019-02-12 20:42:26 +0700

    first Player and Tile script

---
+++ gitTest/Assets/Scripts/Tile.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameObject[] nearbyTiles;
+
+    [SerializeField] private bool empty;
+    [SerializeField] private GameObject ghost;
+    [SerializeField] private GameObject item;
+    private Transform my_transform;
+    private Player player;
+
+    int NoOfAdjacentTiles;
+
+    void Start()
+    {
+        my_transform = GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        NoOfAdjacentTiles = nearbyTiles.Length;
+        if(ghost != null)
+        {
+            empty = false;
+        }
+    }
+
+    public GameObject GetAdjacentTile(int tileNo)
+    {
+        if(tileNo < NoOfAdjacentTiles)
+        {
+            return nearbyTiles[tileNo];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void SetEmpty()
+    {
+        empty = true;
+    }
+
+    public void SetNotEmpty()
+    {
+        empty = false;
+    }
+
+    public bool IsEmpty()
+    {
+        return empty;
+    }
+
+    public void PlaceItem(GameObject newItem)
+    {
+        item = newItem;
+    }
+
+    public void TakeItem()
+    {
+        item = null;
+    }
+
+    void OnMouseDown()
+    {
+        player.SelectTile(gameObject);
+    }
+
+    /*
+    public void DebugGetAllTile()
+    {
+        foreach (GameObject tile in nearbyTiles) {
+            Debug.Log(tile);
+        }
+    }
+    */
+}
