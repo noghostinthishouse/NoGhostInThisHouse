1cfeae894b487aaa9991d81a2c5e9517de5c0258
ChainArtchavit <artchavit_o@sci.tu.ac.th>
2019-04-03 17:50:10 +0700

    title screen fix

---
+++ Engine Proof/New Unity Project/Assets/TitleScreenController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenController : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene("StageSelect");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
