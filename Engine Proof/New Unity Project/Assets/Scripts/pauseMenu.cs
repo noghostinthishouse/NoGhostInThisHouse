66b7f1779852c879a3f6e9c06119ea7f2dd0c9ca
ChainArtchavit <artchavit_o@sci.tu.ac.th>
2019-05-01 12:19:11 +0700

    popup help code - only 1st level for now

--- Engine Proof/New Unity Project/Assets/Scripts/pauseMenu.cs
+++ Engine Proof/New Unity Project/Assets/Scripts/pauseMenu.cs
@@ -14,7 +14,12 @@ public class pauseMenu : MonoBehaviour
     public GameObject ingameMenuUI;
     public GameObject victoryScreen;
     public GameObject loseScreen;
+    public GameObject popupScreen;
+    public GameObject popupImage;
+    public GameObject Xbutton;
+    public Sprite[] popups;
 
+    private int currentPopupPage;
     public GameObject loadingScreen;
     public Slider slider;
 
@@ -22,6 +27,13 @@ public class pauseMenu : MonoBehaviour
     {
         my_player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         player_movement = GameObject.Find("RotatingCollider").GetComponent<PlayerMovement>();
+
+        if (popups.Length > 0)
+        {
+            currentPopupPage = 0;
+            popupImage.GetComponent<Image>().sprite = popups[currentPopupPage];
+            Invoke("showPopups", 1.2f);
+        }
     }
 
     void Update()
@@ -36,6 +48,14 @@ public class pauseMenu : MonoBehaviour
             {
                 Pause();
             }
+        }
+
+        if (popupScreen.activeSelf == true)
+        {
+            popupImage.GetComponent<Image>().sprite = popups[currentPopupPage];
+
+            if (currentPopupPage == popups.Length - 1)
+                Xbutton.SetActive(true);
         }
     }
 
@@ -148,4 +168,37 @@ public class pauseMenu : MonoBehaviour
         }
     }
     */
+
+    public void showPopups()
+    {
+        currentPopupPage = 0;
+        popupImage.GetComponent<Image>().sprite = popups[currentPopupPage];
+        Xbutton.SetActive(false);
+
+        my_player.enabled = false;
+        player_movement.enabled = false;
+
+        popupScreen.SetActive(true);
+        ingameMenuUI.SetActive(false);
+        Time.timeScale = 0f;
+        PlayerTurn.Pause = true;   
+    }
+
+    public void nextPopupPage()
+    {
+        if (currentPopupPage < popups.Length - 1)
+            currentPopupPage++;
+    }
+
+    public void closePopup()
+    {
+        my_player.enabled = true;
+        player_movement.enabled = true;
+        SoundManager.instance.PlaySFX(3);
+
+        popupScreen.SetActive(false);
+        ingameMenuUI.SetActive(true);
+        Time.timeScale = 1f;
+        PlayerTurn.Pause = false;
+    }
 }
