879e75346b85845dfa6f134f118e60b7a1a70d31
wapee <wapee_o@sci.tu.ac.th>
2019-05-15 12:07:13 +0700

    more stable copy action ghost

--- Engine Proof/New Unity Project/Assets/Scripts/CopyActionGhostMovement.cs
+++ Engine Proof/New Unity Project/Assets/Scripts/CopyActionGhostMovement.cs
@@ -28,7 +28,7 @@ public class CopyActionGhostMovement : MonoBehaviour
         {
             angle = playerCol.angle;
             Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+            transform.rotation = rotation;
         }
     }
 
