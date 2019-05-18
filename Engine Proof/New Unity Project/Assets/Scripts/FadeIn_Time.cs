df8fce4b6876b193f93c4494e8bcd0e316563d40
HiKeng <HiKeng@users.noreply.github.com>
2019-05-19 03:09:07 +0700

    Copy-action ghost Sprite Update

---
+++ Engine Proof/New Unity Project/Assets/Scripts/FadeIn_Time.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn_Time : MonoBehaviour
+{
+
+    public float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Expire", timer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Expire (float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
